cc7367c17ccee8056a20c12799f0e2c69ea12d96
Imed <retibi_i@etna-alternance.net>
2022-11-21 21:41:56 +0100

    .

---
+++ My-Portfolio/client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Typical from "react-typical";
+import ScrollService from "../../../utilities/ScrollService";
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="https://github.com/imed57">
+                <i className="fa fa-github" />
+              </a>
+              <a href="https://www.linkedin.com/in/imed-retibi-5595a01a1/">
+                <i className="fa fa-linkedin-square" />
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'm <span className="highlighted-text">IMED</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev 😎",
+                    1000,
+                    "Full stack Developer 💻",
+                    1000,
+                    "Mern stack Dev 📱",
+                    1000,
+                    "Cross Platform Dev 🔴",
+                    1000,
+                    "React/React Native 🌐",
+                    1000,
+                  ]}
+                />
+              </h1>
+            </span>
+            <span className="profile-role-tagline">
+              Knack of building applications with front and back end operations.
+            </span>
+          </div>
+
+          <div className="profile-options">
+            <button className="btn primary-btn" onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+            > Hire Me </button>
+            <a href="CV alternance.pdf" download="RETIBI Imed CV.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
